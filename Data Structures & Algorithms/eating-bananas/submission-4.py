ea71ebac027dd9784d7d71fc559be1974bd77808
Krishna Teja Mellacheruvu <krishnatejamellacheruvu@gmail.com>
2026-09-11 10:08:07 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+        res = right
+
+        while left <= right:
+            k = (left + right)//2
+
+            temp = 0
+
+            for pile in piles:
+                temp+=math.ceil(pile/k)
+            
+            if temp <= h:
+                res = k
+                right = k - 1
+            else:
+                left = k + 1
+        
+        return res
+        
